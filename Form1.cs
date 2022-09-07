115e49b1719641fa94bd2f81cf6dec7e8c73d677
Georgios Zacharidis <zacharidis@icloud.com>
2022-09-07 16:21:18 +0300

    finding a solution for correct IP address form

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace GZScanner
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
+            //check the entered IP for non numeral values and values larger than 255
         {
             int ipFirstPart = int.Parse(textBox3.Text);
 
@@ -22,6 +23,7 @@ namespace GZScanner
             if ((textBox3.Text.Length > 3) | (ipFirstPart > 255) )
             {
                 textBox3.Text="127";
+                
             }
         }
     }
